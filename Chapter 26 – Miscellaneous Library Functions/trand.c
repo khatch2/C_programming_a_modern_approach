38ce3c6cabcd9dc48ae35eb18d38e3743628f622
khatch2_name <20730681+khatch2@users.noreply.github.com>
2024-08-07 15:06:28 +0000

    ss

--- Chapter 26 – Miscellaneous Library Functions/trand.c
+++ Chapter 26 – Miscellaneous Library Functions/trand.c
@@ -5,5 +5,23 @@
 
 int main(void)
  {
-    return 0;
+   int i, seed;
+
+   printf("This program displays the first five values of "
+   "rand.\n");
+
+   for(;;) {
+      for(i = 0; i< 5; i++) {
+         printf("%d ", rand());
+      }
+      printf("\n\n");
+      printf("Enter new seed value (0 to terminate): ");
+      scanf("%d", &seed);
+      if(seed == 0) {
+         break;
+      }
+      srand(seed);
+   }
+
+   return 0;
  }
