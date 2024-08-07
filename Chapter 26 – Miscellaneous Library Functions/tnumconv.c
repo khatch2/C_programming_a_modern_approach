88ae8d645db00d19d1e2c4150625f80af16cf88f
khatch2_name <20730681+khatch2@users.noreply.github.com>
2024-08-07 12:30:54 +0000

    Finished tnumconv.c AND tnumconv.linux

--- Chapter 26 – Miscellaneous Library Functions/tnumconv.c
+++ Chapter 26 – Miscellaneous Library Functions/tnumconv.c
@@ -21,6 +21,24 @@ int main(int argc, char *argv[])
     printf("--------   ------------\n");
     printf("atof       %g\n", atof(argv[1]));
     printf("atoi       %d\n", atoi(argv[1]));
+    printf("atol       %ld\n\n", atol(argv[1]));
+
+    printf("Function   Return Value  Valid?   "
+           "String Consumed?\n"
+           "-------- -------------- -------   "
+           "----------------\n");
+
+    errno = 0;
+    printf("strtod     %-12g", strtod(argv[1], &ptr));
+    CHK_VALID;
+
+    errno = 0;
+    printf("strtol     %-12ld", strtol(argv[1], &ptr, 10));
+    CHK_VALID;
+
+    errno = 0;
+    printf("strtoul     %-12lu", strtoul(argv[1], &ptr, 10));
+    CHK_VALID;
 
     return 0;
 }
