3e22e4de8b98ffbb5e14956162cdd332b7a57f81
khatch2_name <20730681+khatch2@users.noreply.github.com>
2024-08-08 12:08:38 +0000

    second try of writing airmiles.c

--- Chapter 26 – Miscellaneous Library Functions/airmiles.c
+++ Chapter 26 – Miscellaneous Library Functions/airmiles.c
@@ -1,5 +1,17 @@
 /* */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct city_info {
+    char *city;
+    int miles;
+};
+
+int compare_cities( const void *key_ptr,
+                    const void *element_ptr);
+                    
 int main(void)
 {
     return 0;
