4fb8b069486eabd8f924fb9731efb3e1bf1b8f15
khatch2_name <20730681+khatch2@users.noreply.github.com>
2024-08-08 15:06:42 +0000

    updated tsignal.c AND tsignal.linux

--- Chapter 24 – Error Handling/tsignal.c
+++ Chapter 24 – Error Handling/tsignal.c
@@ -1,4 +1,5 @@
 /* */
+
 #include <signal.h>
 #include <stdio.h>
 
@@ -7,30 +8,30 @@ void raise_sig(void);
 
 int main(void)
 {
-    void (*orig_handler)(int);
+  void (*orig_handler)(int);
 
-    printf("Installing handler for signal %d\n", SIGINT);
-    orig_handler = signal(SIGINT, handler);
-    raise_sig();
+  printf("Installing handler for signal %d\n", SIGINT);
+  orig_handler = signal(SIGINT, handler);
+  raise_sig();
 
-    printf("Changing handler to SIG_IGN\n");
-    signal(SIGINT, SIG_IGN);
-    raise_sig();
+  printf("Changing handler to SIG_IGN\n");
+  signal(SIGINT, SIG_IGN);
+  raise_sig();
 
-    printf("restoring original handler\n");
-    signal(SIGINT, orig_handler);
-    raise_sig();
+  printf("Restoring original handler\n");
+  signal(SIGINT, orig_handler);
+  raise_sig();
 
-    printf("Program terminates normally\n");
-    return 0;
+  printf("Program terminates normally\n");
+  return 0;
 }
 
 void handler(int sig)
 {
-    printf("Handler called for signal %d\n", sig);
+  printf("Handler called for signal %d\n", sig);
 }
 
 void raise_sig(void)
 {
-    raise(SIGINT);
+  raise(SIGINT);
 }
