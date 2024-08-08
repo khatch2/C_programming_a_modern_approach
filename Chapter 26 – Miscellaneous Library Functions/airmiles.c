c8a888bbce05c094f2bb0c9b40dad5338a4f80ea
khatch2_name <20730681+khatch2@users.noreply.github.com>
2024-08-08 14:00:20 +0000

    Successfully created airmiles.c AND airmiles.linux

--- Chapter 26 – Miscellaneous Library Functions/airmiles.c
+++ Chapter 26 – Miscellaneous Library Functions/airmiles.c
@@ -5,19 +5,19 @@
 #include <string.h>
 
 struct city_info {
-    char *city;
-    int miles;
+  char *city;
+  int miles;
 };
 
-int compare_cities( const void *key_ptr,
-                    const void *element_ptr);
-                    
+int compare_cities(const void *key_ptr,
+                   const void *element_ptr);
+
 int main(void)
 {
-    char city_name[81];
-    struct city_info *ptr;
-    const struct city_info mileage[] = {
-     {"Berlin",         3965}, {"Buenos Aires", 5297},
+  char city_name[81];
+  struct city_info *ptr;
+  const struct city_info mileage[] =
+    {{"Berlin",         3965}, {"Buenos Aires", 5297},
      {"Cairo",          5602}, {"Calcutta",     7918},
      {"Cape Town",      7764}, {"Caracas",      2132},
      {"Chicago",         713}, {"Hong Kong",    8054},
@@ -30,30 +30,25 @@ int main(void)
      {"San Francisco",  2571}, {"Shanghai",     7371},
      {"Stockholm",      3924}, {"Sydney",       9933},
      {"Tokyo",          6740}, {"Warsaw",       4344},
-     {"Washington",      205}
-    };
+     {"Washington",      205}};
 
-    printf("Enter city name: ");
-    scanf("%80[\n]", city_name);
-    ptr = bsearch(
-        city_name,
-        mileage,
-        sizeof(mileage) / sizeof(mileage[0]),
-        sizeof(mileage[0]),
-        compare_cities
-        );
-    if (ptr != NULL) {
-        printf("%s is %d miles from New York City.\n", city_name, ptr->miles);
-    } else {
-        printf("%s wasn't found.\n", city_name);
-    }
-    
-    return 0;
+  printf("Enter city name: ");
+  scanf("%80[^\n]", city_name);
+  ptr = bsearch(city_name, mileage,
+                sizeof(mileage) / sizeof(mileage[0]),
+                sizeof(mileage[0]), compare_cities);
+  if (ptr != NULL)
+    printf("%s is %d miles from New York City.\n",
+           city_name, ptr->miles);
+  else
+    printf("%s wasn't found.\n", city_name);
+
+  return 0;
 }
 
-int compare_cities(  const void *key_ptr,
-                     const void *element_ptr)
+int compare_cities(const void *key_ptr,
+                   const void *element_ptr)
 {
-        return strcmp( (char *) key_ptr,
-                    ( (struct city_info *) element_ptr)->city);
+  return strcmp((char *) key_ptr,
+                ((struct city_info *) element_ptr)->city);
 }
